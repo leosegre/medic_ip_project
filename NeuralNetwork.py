e02c4ac3ab33e0b78a7dab477454df9910cc9818
yarden haskin <yardenhaskin93@gmail.com>
2021-06-09 19:50:13 +0300

    work

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -13,12 +13,20 @@ class NeuralNetwork():
         self.f1 = f1
         self.f2 = f2
         self.z1 = None
-        self.a1= None
+        self.a1 = None
         self.z2 = None
-        self.a2  = None
+        self.a2 = None
+        self.loss = None
 
     def sigmoid(self, x):
         return 1.0 / (1.0 + np.exp(-x))
+
+    def sigmoid_derivative(self, z):
+        return self.sigmoid(z)* (1-self.sigmoid(z))
+
+
+    def tanh_derivative(self, z):
+       return 1/(np.cosh(z)**2)
 
     def tanh(self, x):
         return np.tanh(x)
@@ -49,11 +57,17 @@ class NeuralNetwork():
         elif self.f1 == "relu":
             return self.relu(self.z1)
 
-    def forward_pass(self, input):
+    def loss_function(self, predicted, labels):
+       return (((labels-predicted)**2)/2).mean()
+
+    def forward_pass(self, input, labels):
         self.z1 = self.calculate_linear_combination(input, self.W1.transpose(), self.b1)
         self.a1 = self.apply_activation(self.f1)
         self.z2 = self.calculate_linear_combination(self.a1, self.W2.transpose(), self.b2)
         self.a2 = self.apply_activation(self.f2)
+        self.loss = self.loss_function(self.f2, labels)
+
+
 
 
 
