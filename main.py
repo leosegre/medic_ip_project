5df545a6acf43f727bf44648bf08f62687daa01e
leo segre <leosegre@gmail.com>
2021-06-09 17:21:34 +0300

    images

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import numpy as np
+
+
 batch_size = 1
 nn_hdim = 1024
+train_data = []
+val_data = []
+train_label = []
+val_label = []
+
+load_image()
+    image = np.load
+
+def load_data():
 
 
 
 def main():
-    print("check")
+    load_data()
     
 
 if __name__ == "__main__":
